e8c882c37665a558e73ff2d29df14e5c3a853f48
cuong cuong <01239586229c@gmail.com>
2022-03-20 09:43:41 +0700

    account

--- Xedap/Controllers/AccountController.cs
+++ Xedap/Controllers/AccountController.cs
@@ -16,6 +16,37 @@ namespace Xedap.Controllers
         public ActionResult Login()
         {
             return View();
+
+        }
+        public ActionResult Register()
+        {
+            return View();
+
+        }
+        public ActionResult ForgotPassword()
+        {
+            return View();
+
+        }
+        public ActionResult ResetPassword()
+        {
+            return View();
+
+        }
+        public ActionResult EmailConfirmCompleted()
+        {
+            return View();
+        }
+        //[HttpPost]
+        /*public ActionResult SendConfirmMailAgain(string idStaff)
+        {
+            var Staff = context.AccountStaffs.FirstOrDefault(p => p.IDStaff == idStaff);
+            var result = accountStaffRepo.ResendEmail(context, Staff);
+            return Json(result);
+        }*/
+        public ActionResult SendMailAgain()
+        {
+            return View();
         }
     }
 }
