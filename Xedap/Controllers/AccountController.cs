e5a3320089abf4a9f9e4c83ed65c22b4e8078d8e
unknown <truongbao901@gmail.com>
2022-03-21 23:09:50 +0700

    account

--- Xedap/Controllers/AccountController.cs
+++ Xedap/Controllers/AccountController.cs
@@ -6,12 +6,33 @@ using System.Web.Mvc;
 
 namespace Xedap.Controllers
 {
-    public class AccountController : Controller
+    
+    public class AccountController : BaseController
     {
+        
         // GET: Account
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Address()
+        {
+            return View(DataContext.Addresses);
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            //var address = AddressRepo.GetAddressByUser(context, userId);
+            //ViewBag.Address = address;
+            //ViewBag.AddressCount = address.Count;
+            return View(DataContext.Carts);
+        }
+        public ActionResult Invoice()
+        {
+            return View(DataContext.Invoices);
         }
         public ActionResult Login()
         {
